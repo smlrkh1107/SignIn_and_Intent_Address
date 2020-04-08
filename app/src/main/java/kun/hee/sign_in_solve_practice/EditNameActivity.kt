cdc010588225e71a9191a475dec4cd4ea5f7ffa1
smlrkh1107 <krae7789230@gmail.com>
2020-04-08 22:07:27 +0900

    startActivityForResult 예제

--- app/src/main/java/kun/hee/sign_in_solve_practice/EditNameActivity.kt
+++ app/src/main/java/kun/hee/sign_in_solve_practice/EditNameActivity.kt
@@ -3,13 +3,31 @@ package kun.hee.sign_in_solve_practice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_adress.*
 import kotlinx.android.synthetic.main.activity_edit_name.*
 
 class EditNameActivity : AppCompatActivity() {
 
+    // 이 1000이라는 숫자는, 주소를 얻기위한 요청이라고 하는 메모.
+    val REQ_FOR_ADDRESS = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_name)
+
+
+        detailAddressBtn.setOnClickListener {
+            val myIntent = Intent(this, EditAdressActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_ADDRESS)
+            //결과받는거
+            //requestCode 에 바로 숫자를 넣으면 뭘 의미하는지 잊기 딱좋지?
+            // 그래서 class 아래에 변수 지정
+
+        }
+
+
+
+
 
         nameOkBtn.setOnClickListener {
 
