bcbdc1c930c08127e8e2c7bfe87ba913d2b8f9e2
smlrkh1107 <krae7789230@gmail.com>
2020-04-08 21:10:11 +0900

    [error]다음버튼 누르면 마이페이지로 이동하는데

--- app/src/main/java/kun/hee/sign_in_solve_practice/MainActivity.kt
+++ app/src/main/java/kun/hee/sign_in_solve_practice/MainActivity.kt
@@ -1,12 +1,24 @@
 package kun.hee.sign_in_solve_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //다음버튼이름이지지
+       nextBtn.setOnClickListener {
+            //MyPageActivity로 이동시켜주고 싶꾸나
+
+           //다음버튼 누르면 인텐트로~~ 여기서부터 class자바
+           val myIntent = Intent(this, MyPageActivity::class.java)
+           startActionMode(myIntent)
+        }
+
     }
 }
