297e9573cd384a81d2a0d4e64ab056ff7bb4e64b
smlrkh1107 <krae7789230@gmail.com>
2020-04-08 21:51:11 +0900

    activity간 데이터 첨부  - string / Int 보낼 때의 차이(int만 디폴트값 설정 필요)

--- app/src/main/java/kun/hee/sign_in_solve_practice/EditNameActivity.kt
+++ app/src/main/java/kun/hee/sign_in_solve_practice/EditNameActivity.kt
@@ -14,10 +14,14 @@ class EditNameActivity : AppCompatActivity() {
         nameOkBtn.setOnClickListener {
 
             val inputName = nameEdt.text.toString()
+            val inputBirthYear = birthYear.text.toString().toInt() // 자료형 변환이 쉬워용
 
             val myIntent = Intent(this, MyPageActivity::class.java)
             //데이터가져와라아아
+
             myIntent.putExtra("userName",inputName)
+            myIntent.putExtra("userAge",2020-inputBirthYear+1) //실제나이나오게끔 계산. - intenger로 담긴다.
+
             startActivity(myIntent)
         }
     }
--- app/src/main/java/kun/hee/sign_in_solve_practice/MyPageActivity.kt
+++ app/src/main/java/kun/hee/sign_in_solve_practice/MyPageActivity.kt
@@ -11,7 +11,11 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
         val name = intent.getStringExtra("userName") //보낼때 정한 이름
-        userNameText.text = name
+//        userNameText.text = name + "님 안녕하세요"
+
+        val age = intent.getIntExtra("userAge", 0)
+        // ★★intenger는 값을 받아오지 않으면 Default를 꼭 ★ 정해줘야 해, string은 null들어가
+        userNameText.text = "${name}님,  ${age}세"
 
 
     }
