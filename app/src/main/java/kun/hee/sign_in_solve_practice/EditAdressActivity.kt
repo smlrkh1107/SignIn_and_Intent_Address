4dab6ec3dcf53617c44ee39b8e2fdf1f07c80b86
smlrkh1107 <krae7789230@gmail.com>
2020-04-08 21:58:37 +0900

    상세 주소 입력 화면

---
+++ app/src/main/java/kun/hee/sign_in_solve_practice/EditAdressActivity.kt
@@ -0,0 +1,12 @@
+package kun.hee.sign_in_solve_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAdressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_adress)
+    }
+}
